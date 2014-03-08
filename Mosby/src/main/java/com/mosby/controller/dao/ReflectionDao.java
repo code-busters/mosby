1520a0654ac0e5e95f205e121715916017efe0bd
Olesko <olesko_lp@ukr.net>
2014-03-09 00:08:08 +0200

    DAO fix

--- Mosby/src/main/java/com/mosby/controller/dao/ReflectionDao.java
+++ Mosby/src/main/java/com/mosby/controller/dao/ReflectionDao.java
@@ -1,6 +1,5 @@
 package main.java.com.mosby.controller.dao;
 
-import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -10,18 +9,19 @@ import java.util.List;
 
 import main.java.com.mosby.controller.persistence.ConnectionManager;
 import main.java.com.mosby.controller.transformers.ReflectionTransformer;
-import main.java.com.mosby.utils.StringUtils;
 
 public class ReflectionDao<T> {
 
 	private Class<T> type;
 	private String query;
 	private ReflectionTransformer<T> reflectionTransformer;
+	private QueryStatements<T> queryStatements;
 
 	public ReflectionDao(Class<T> type) {
 		this.type = type;
 		this.reflectionTransformer = new ReflectionTransformer<>();
-		this.query = createQuery();
+		this.queryStatements = new QueryStatements<>(type);
+		this.query = null;
 	}
 
 	public Class<T> getType() {
@@ -36,61 +36,28 @@ public class ReflectionDao<T> {
 		return query;
 	}
 
-	public void setQuery() {
-		this.query = createQuery();
+	public void setQuery(String queryStatement) {
+		this.query = queryStatement;
 	}
-
-	public String createQuery() {
-		StringBuilder stringBuilder = new StringBuilder();
-
-		stringBuilder.append("SELECT ");
-		stringBuilder.append(getColumns(false));
-		stringBuilder.append("FROM ");
-
-		stringBuilder.append(
-				reflectionTransformer.fromFieldToColumnInDB(type
-						.getSimpleName())).append("s");
-		System.out.println(type.getSimpleName());
-
-		return stringBuilder.toString();
-	}
-
-	// -------------------SELECT QUERY WITH WHERE STATEMENT------------
-	public String createSelectQuery(String fieldName, Object whereObj) {
-		String query = null;
-
-		String tableName = reflectionTransformer.fromFieldToColumnInDB(type
-				.getSimpleName()) + "s";
-		String tableColumns = getColumns(false);
-		String whereColumn = reflectionTransformer
-				.fromFieldToColumnInDB(fieldName);
-		String whereValue = whereObj.toString();
-
-		query = StringUtils.concat("SELECT ", tableColumns, " FROM ",
-				tableName, " WHERE ", whereColumn, "='", whereValue, "'");
-
-		System.out.println(query);
-
-		return query;
-	}
-
-	// -----------------------------------------------------------------
 
 	public List<T> selectObjects(String fieldName, Object whereObj) {
 		List<T> objects = new ArrayList<>();
 		try {
-			PreparedStatement stmt = (PreparedStatement) ConnectionManager
-					.getInstance().getConnection()
-					.prepareStatement(createSelectQuery(fieldName, whereObj));
-			ResultSet rs = stmt.executeQuery();
+			query = queryStatements.createSelectQuery(fieldName, whereObj);
+			
+			Connection connection = ConnectionManager.getInstance()
+					.getConnection();
+			PreparedStatement preparedStatement = (PreparedStatement) connection
+					.prepareStatement(query);
+			ResultSet resultSet = preparedStatement.executeQuery();
 
-			while (rs.next()) {
+			while (resultSet.next()) {
 				objects.add(reflectionTransformer.fromRStoObject(
-						type.newInstance(), rs, type));
+						type.newInstance(), resultSet, type));
 			}
-			rs.close();
-			stmt.close();
 
+			resultSet.close();
+			preparedStatement.close();
 		} catch (ClassNotFoundException | SQLException | InstantiationException
 				| IllegalAccessException e) {
 			e.printStackTrace();
@@ -98,55 +65,26 @@ public class ReflectionDao<T> {
 		return objects;
 	}
 
-	// -------------------INSERT QUERY WITH VALUES---------------------
-	public String createInsertQuery() {
-		String query = null;
-
-		String tableName = reflectionTransformer.fromFieldToColumnInDB(type
-				.getSimpleName()) + "s";
-		String tableColumns = getColumns(true);
-
-		query = StringUtils.concat("INSERT INTO ", tableName, " VALUES (",
-				tableColumns, ")");
-
-		System.out.println(query);
-
-		return query;
-	}
-
-	// ----------------------------------------------------------------
-
 	public void insertObjects(T object) {
-		PreparedStatement preparedStatement;
 		try {
+			query = queryStatements.createInsertQuery();
+			
 			Connection connection = ConnectionManager.getInstance()
 					.getConnection();
-			preparedStatement = (PreparedStatement) connection
-					.prepareStatement(createInsertQuery());
-
+			PreparedStatement preparedStatement = (PreparedStatement) connection
+					.prepareStatement(query);
+			
 			preparedStatement = (PreparedStatement) reflectionTransformer
 					.fromObjectToStatement(preparedStatement, type, object);
 
 			preparedStatement.addBatch();
 			preparedStatement.executeBatch();
+
+			preparedStatement.close();
 		} catch (SQLException | IllegalArgumentException
 				| ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
 
-	private String getColumns(boolean hasValues) {
-		StringBuilder stringBuilder = new StringBuilder();
-
-		for (Field field : type.getDeclaredFields()) {
-			if (hasValues) {
-				stringBuilder.append("?, ");
-			} else {
-				stringBuilder.append(field.getName()).append(", ");
-			}
-		}
-		stringBuilder.deleteCharAt(stringBuilder.length() - 2);
-		return reflectionTransformer.fromFieldToColumnInDB(stringBuilder
-				.toString());
-	}
 }
--- Mosby/src/main/java/com/mosby/controller/services/MainService.java
+++ Mosby/src/main/java/com/mosby/controller/services/MainService.java
@@ -15,12 +15,8 @@ public class MainService {
 				.getClass();
 		System.out.println(typeBaseUserInfo);
 
-		//usersDao.selectObjects("id",2);
-		System.out.println(usersDao.selectObjects("id",2));
-		//usersDao.selectObjects("firstName", "Oleh");
-		System.out.println(usersDao.selectObjects("firstName", "Oleh"));
-		//usersDao.selectObjects("admin", 1);
-		System.out.println(usersDao.selectObjects("admin", 1));
+		System.out.println(usersDao.selectObjects("admin", true));
+		System.out.println(usersDao.selectObjects("firstName", "Oleksii"));
 	}
 
 	public void insertUser(BaseUserInfo baseUserInfo) {
