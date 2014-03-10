2d3173c9a8a5770f7c00d0938e1d7f8faee7ddb0
Olesko <olesko_lp@ukr.net>
2014-03-10 22:58:02 +0200

    DAO updateObjects, createUpdateQuery & mods

--- Mosby/src/main/java/com/mosby/controller/dao/QueryStatements.java
+++ Mosby/src/main/java/com/mosby/controller/dao/QueryStatements.java
@@ -18,15 +18,19 @@ public class QueryStatements<T> {
 	public String createSelectQuery(String fieldName, Object whereObj) {
 		String query = null;
 
-		String tableName = reflectionTransformer.fromFieldToColumnInDB(type.getSimpleName()) + "s";
+		String tableName = reflectionTransformer.fromFieldToColumnInDB(type
+				.getSimpleName()) + "s";
 		String tableColumns = getColumns(false);
 
 		if (fieldName.equals("")) {
-			query = StringUtils.concat("SELECT ", tableColumns, "FROM ", tableName);
+			query = StringUtils.concat("SELECT ", tableColumns, "FROM ",
+					tableName);
 		} else {
-			String whereColumn = reflectionTransformer.fromFieldToColumnInDB(fieldName);
+			String whereColumn = reflectionTransformer
+					.fromFieldToColumnInDB(fieldName);
 			String whereValue = whereObj.toString();
-			query = StringUtils.concat("SELECT ", tableColumns, "FROM ", tableName, " WHERE ", whereColumn, "='", whereValue, "'");
+			query = StringUtils.concat("SELECT ", tableColumns, "FROM ",
+					tableName, " WHERE ", whereColumn, "='", whereValue, "'");
 		}
 
 		System.out.println(query);
@@ -49,21 +53,33 @@ public class QueryStatements<T> {
 		return query;
 	}
 
-	public String createUpdateQuery() {
-		// final String UPDATE_BOOK_AMOUNT =
-		// "UPDATE Books SET amount=? WHERE id=?";
+	public String createUpdateQuery(String whereField) {
 		String query = null;
 
 		String tableName = reflectionTransformer.fromFieldToColumnInDB(type
 				.getSimpleName()) + "s";
-		String tableColumns = getColumns(true);
+		String tableColumns = getUpdateColumns(whereField);
 
-		query = StringUtils.concat("UPDATE ", tableName, " SET ", tableColumns,
-				")");
+		query = "UPDATE " + tableName + " SET " + tableColumns + "WHERE "
+				+ whereField + "=?";
 
 		System.out.println(query);
 
 		return query;
+	}
+
+	private String getUpdateColumns(String whereField) {
+		StringBuilder stringBuilder = new StringBuilder();
+
+		for (Field field : type.getDeclaredFields()) {
+			String fieldName = reflectionTransformer.fromFieldToColumnInDB(
+					field.getName()).toString();
+			if (!whereField.equals(fieldName)) {
+				stringBuilder.append(fieldName).append("=?, ");
+			}
+		}
+		stringBuilder.deleteCharAt(stringBuilder.length() - 2);
+		return stringBuilder.toString();
 	}
 
 	private String getColumns(boolean hasValues) {
